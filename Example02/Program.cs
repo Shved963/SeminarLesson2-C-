69669ae3dd82157315a61e93c465df1039f897bb
Andrei Shvetsov <shved963@gmail.com>
2022-11-30 16:19:37 +0300

    Example02_delete midl number

---
+++ Example02/Program.cs
@@ -0,0 +1,14 @@
+﻿//1. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46 782 -> 72 918 -> 98
+
+Console.Clear();
+
+Random random = new Random();
+int number = random.Next(100, 1000);
+
+int firstNumber = number / 100;
+int lastNumber = number % 10;
+Console.Write(number);
+Console.Write("-> ");
+Console.Write(firstNumber);
+Console.Write(lastNumber);
