f1b9b9a7ea75cd0f2bde2fb53fb31df749d7f04d
Andrei Shvetsov <shved963@gmail.com>
2022-11-30 16:36:49 +0300

    Example03_multiple_or_not

---
+++ Example03/Program.cs
@@ -0,0 +1,29 @@
+﻿//2.Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+//Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4 16, 4 -> кратно
+
+
+Console.Clear();
+Console.WriteLine("Введите два числа");
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+int lastNum;
+
+if (numA % numB == 0)
+{
+    Console.Write(numA);
+    Console.Write(", ");
+    Console.Write(numB);
+    Console.Write(" = кратно");
+}
+else
+{
+    lastNum = numA % numB;
+
+    Console.Write(numA);
+    Console.Write(", ");
+    Console.Write(numB);
+    Console.Write(" = не кратно, остаток ");
+    Console.Write(lastNum);
+}
