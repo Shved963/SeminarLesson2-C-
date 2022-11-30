12867c84ee5af53ea4d2fc2760925cc7681913a9
Andrei Shvetsov <shved963@gmail.com>
2022-11-30 17:15:44 +0300

    Example04_multiple_7&23

---
+++ Example04/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+Console.Clear();
+
+int num = int.Parse(Console.ReadLine());
+
+int multiple1 = 7;
+int multiple2 = 23;
+
+if (num % multiple1 == 0 & num % multiple2 == 0)
+{
+    Console.WriteLine($"Число {num} кратно {multiple1} и {multiple2}");
+}
+else
+{
+    Console.WriteLine($"Число {num} НЕ КРАТНО {multiple1} и {multiple2}");
+}
