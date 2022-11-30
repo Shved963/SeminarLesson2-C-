0b01fe42538fa305fb0973c5a439fbe9eb7ff3e5
Andrei Shvetsov <shved963@gmail.com>
2022-11-30 16:07:52 +0300

    Random

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99]
+//и показывает наибольшую цифру числа.
+//78-> 8
+//12-> 2
+//85-> 8
+
+Console.Clear();
+
+Random random = new Random();
+int number = random.Next(10, 100);
+
+int firstNumber = number/10;
+int lastNumber = number%10;
+
+if (firstNumber > lastNumber)
+{
+    Console.Write(number);
+    Console.Write("-> ");
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.Write(number);
+    Console.Write("-> ");
+    Console.WriteLine(lastNumber);
+}
+//Console.WriteLine(number);
+//Console.WriteLine(firstNumber);
+//Console.WriteLine(lastNumber);
